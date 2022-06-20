0ad4aefff62e794f02ed1584cd730f71b6d4b75b
rhl120 <rhl120@protonmail.com>
2022-06-20 18:00:53 +0200

    Add basic functionality to rhfind

--- rhfind/rhfind.go
+++ rhfind/rhfind.go
@@ -1,7 +1,50 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+type config struct {
+	sp  []string
+	sep bool
+}
+
+func loadConfig() (*config, error) {
+	var cfg config
+	flag.BoolVar(&cfg.sep, "sep", false,
+		"Add a seprator between starting paths")
+	flag.Parse()
+	args := flag.Args()
+	if len(args) < 1 {
+		cfg.sp = make([]string, 1)
+		cfg.sp[0] = "."
+	} else {
+		cfg.sp = flag.Args()
+	}
+	return &cfg, nil
+}
 
 func main() {
-	fmt.Println("Hello from rhfind!")
+	cfg, err := loadConfig()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+	}
+	f := func(path string, entry os.FileInfo, err error) error {
+		fmt.Println(path)
+		return nil
+	}
+	for idx, i := range cfg.sp {
+
+		if err := filepath.Walk(i, filepath.WalkFunc(f)); err != nil {
+			fmt.Printf("Failed to walk through directory %s, error: %v\n",
+				i, err)
+
+		}
+		if idx != len(cfg.sp)-1 && cfg.sep {
+			fmt.Println("----")
+		}
+	}
 }
