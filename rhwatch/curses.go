cd55b716fd800d57bcab28945e82555277994876
rhl120 <rhl120@protonmail.com>
2022-06-24 19:23:37 +0200

    Add curses

---
+++ rhwatch/curses.go
@@ -0,0 +1,39 @@
+//This is in a seprate file because currently gopls does not work well with
+// cgo
+package main
+
+//#cgo LDFLAGS: -lcurses
+//#include <curses.h>
+//#include <stdlib.h>
+//#include <stdio.h>
+//void curses_write(const char *str){
+//printw("%s", str);
+//}
+import "C"
+import (
+	"fmt"
+	"unsafe"
+)
+
+func cursesInit() {
+	C.initscr()
+}
+
+func cursesWrite(str string) {
+	cstr := C.CString(str)
+	C.curses_write(cstr)
+	C.free(unsafe.Pointer(cstr))
+}
+
+func cursesWritef(format string, args ...interface{}) {
+	cursesWrite(fmt.Sprintf(format, args...))
+}
+
+func cursesClear() {
+	C.clear()
+	C.refresh()
+}
+
+func cursesClean() {
+	C.endwin()
+}
